09967bfe8570f222d5cae0b88d1a4fe380be6ded
huangwei01 <huangwei01@itiger.com>
2020-10-11 23:34:33 +0800

    mybatis文件结构修改

--- src/main/java/com/yuepaijie/YuePaiJieApplication.java
+++ src/main/java/com/yuepaijie/YuePaiJieApplication.java
@@ -1,9 +1,11 @@
 package com.yuepaijie;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yuepaijie.common.dao.generated")
 public class YuePaiJieApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/yuepaijie/controller/UserInfoController.java
+++ src/main/java/com/yuepaijie/controller/UserInfoController.java
@@ -1,17 +1,26 @@
 package com.yuepaijie.controller;
 
-import com.yuepaijie.common.vo.RestEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-@ResponseBody
+import com.yuepaijie.common.entity.generated.UserAccount;
+import com.yuepaijie.common.vo.RestEntity;
+import com.yuepaijie.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 @RequestMapping("/userInfo")
 public class UserInfoController {
-  public RestEntity addOrEditUser(){
 
+  @Autowired UserInfoService userInfoService;
+
+  @PostMapping("/addoredit")
+  public RestEntity addOrEditUser(@RequestBody UserAccount userAccount){
+    userInfoService.addOrModify(userAccount);
     RestEntity restEntity = new RestEntity();
-    return null;
+    restEntity.setCode(200);
+    return restEntity;
   }
 }

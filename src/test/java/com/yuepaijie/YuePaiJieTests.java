a056e06a9e54c09d1d8beb2e4713ab9553cff794
黄伟 <554610288@qq.com>
2020-10-15 23:14:41 +0800

    配置redis

--- src/test/java/com/yuepaijie/YuePaiJieTests.java
+++ src/test/java/com/yuepaijie/YuePaiJieTests.java
@@ -1,14 +1,21 @@
 package com.yuepaijie;
 
+import com.yuepaijie.common.tool.redis.RedisOperator;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class YuePaiJieTests {
 
+    @Autowired
+    RedisOperator redisOperator;
+
     @Test
-    void contextLoads() {
-        int c;
+    void contextLoads() throws Exception{
+        redisOperator.set("11","22",10);
+        Thread.sleep(500);
+        System.out.println(redisOperator.get("11"));
     }
 
 }
